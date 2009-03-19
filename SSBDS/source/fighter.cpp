6db2f3b2c55b6dc869e321ca4c41bf7fc18a2ea0
Jeremy Vercillo <icelax1992@comcast.net>
2009-03-18 20:35:28 -0400

    Fixed a stupid error with.

--- SSBDS/source/fighter.cpp
+++ SSBDS/source/fighter.cpp
@@ -107,7 +107,7 @@ void Fighter::initSprite() {
 	PA_LoadSpritePal(MAIN_SCREEN, 14-(SPRITENUM-100), (void*)shield_Pal);
 	PA_CreateSprite(MAIN_SCREEN, 30+(SPRITENUM-100), (void*)shield_Sprite, OBJ_SIZE_64X64, COLOR256, 14-(SPRITENUM-100), -64, -64);
 	PA_SetSpriteDblsize(MAIN_SCREEN, 30+(SPRITENUM-100), 1);
-	PA_SetRotsetNoAngle(MAIN_SCREEN, 30+(SPRITENUM-100), 128);
+	PA_SetRotsetNoAngle(MAIN_SCREEN, 30+(SPRITENUM-100), 128, 128);
 	PA_SetSpriteRotEnable(MAIN_SCREEN, 30+(SPRITENUM-100), SPRITENUM-100);
 }
 void Fighter::initFrames(){}//implemented in subclasses
